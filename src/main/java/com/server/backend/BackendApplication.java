28b44d0f98d6ea457ddfe06538555ba2d63d642b
Ramyamylapalli <mylapalliramya147@gmail.com>
2026-06-10 15:48:27 +0530

    database connected

--- src/main/java/com/server/backend/BackendApplication.java
+++ src/main/java/com/server/backend/BackendApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-	//changed java version to 21
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
