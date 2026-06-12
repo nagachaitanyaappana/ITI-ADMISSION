585313dcf4f6fbaa07d01a6be3a15dab0029c5b7
nagachaitanyaappana-cloud <nagachaitanyaappana@gmail.com>
2026-06-12 12:08:07 +0530

    written itirepository

--- src/main/java/com/server/backend/controller/ReportController.java
+++ src/main/java/com/server/backend/controller/ReportController.java
@@ -7,55 +7,55 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.server.backend.DTO.Reports.DistrictNameResponse;
 import com.server.backend.DTO.Reports.DistrictCollegeTypeResponse;
+import com.server.backend.DTO.Reports.DistrictNameResponse;
+import com.server.backend.DTO.Reports.ITINameResponse;
 import com.server.backend.Repository.ItiRepository;
 import com.server.backend.Repository.districtmasterrepo;
-import com.server.backend.DTO.Reports.ITINameResponse;
 
 @RestController
 public class ReportController {
 
-        private final districtmasterrepo districtmasterrepo;
-        private final ItiRepository itiRepository;
-        public ReportController(districtmasterrepo districtmasterrepo,ItiRepository itiRepository) {
-            this.districtmasterrepo = districtmasterrepo;
-            this.itiRepository = itiRepository;
-        }
+    private final districtmasterrepo districtmasterrepo;
+    private final ItiRepository itiRepository;
+    public ReportController(districtmasterrepo districtmasterrepo,ItiRepository itiRepository) {
+    this.districtmasterrepo = districtmasterrepo;
+        this.itiRepository = itiRepository;
+    }
 
+    @GetMapping("/districts")
+    public DistrictNameResponse getDistricts() {
+        List<String> districtNames = districtmasterrepo.findAllNames();
 
-            @GetMapping("/districts")
-            public DistrictNameResponse getDistricts() {
-                List<String> districtNames = districtmasterrepo.findAllNames();
+        DistrictNameResponse names = new DistrictNameResponse();
+        names.setDistrictName(districtNames);
 
-                DistrictNameResponse names = new DistrictNameResponse();
-                names.setDistrictName(districtNames);
+        return names;
+    }
 
-                return names;
-            }
+    @PostMapping("/districts/code")
+        public ITINameResponse getItiNamesByDistrict(@RequestBody DistrictCollegeTypeResponse districtCollegeType) {
 
-            @PostMapping("/districts/code")
-            public ITINameResponse getItiNamesByDistrict(@RequestBody DistrictCollegeTypeResponse districtCollegeType) {
+        String dist = districtCollegeType.getDist();
 
-            String dist = districtCollegeType.getDist();
+        String dist_code = districtmasterrepo.findCodeByDistrictName(dist);
 
-            String dist_code = districtmasterrepo.findCodeByDistrictName(dist);
+        List<String> iti_names =   itiRepository.findItiNamesByDistrictCode(dist_code);
 
-            List<String> iti_names =   itiRepository.findItiNamesByDistrictCode(dist_code);
+        ITINameResponse response = new ITINameResponse();
+        response.setItiNames(iti_names);
 
-            ITINameResponse response = new ITINameResponse();
-            response.setItiNames(iti_names);
-
-            return response;
-        }
+        return response;
+    }
         
-        @GetMapping("/AboutStrive")
-        public String aboutStrive() {
-            return "About Strive.";
-        }
+    @GetMapping("/AboutStrive")
+    public String aboutStrive() {
+        return "About Strive.";
+    }
 
-         @GetMapping("/DisclosureManagement")
-         public String disclosureManagement() {
-            return "Disclosure Management.";
-        }
+    @GetMapping("/DisclosureManagement")
+    public String disclosureManagement() {
+        return "Disclosure Management.";
+    }
+    
 }
