315145d4b6dd76e0412c95721d585a8a69f83615
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-13 23:28:27 +0530

    gg

--- src/main/java/com/server/backend/controller/MeritListController.java
+++ src/main/java/com/server/backend/controller/MeritListController.java
@@ -1,12 +1,9 @@
 package com.server.backend.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.server.backend.entity.MeritList;
 import com.server.backend.service.MeritListService;
 
