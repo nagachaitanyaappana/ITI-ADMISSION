1143e9793b6f6e63deb3abb7ab2e65d79452d1f3
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-14 23:46:58 +0530

    jyg

--- src/main/java/com/server/backend/controller/ChecklistController.java
+++ src/main/java/com/server/backend/controller/ChecklistController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.server.backend.entity.Checklist;
 import com.server.backend.service.ChecklistService;
 
