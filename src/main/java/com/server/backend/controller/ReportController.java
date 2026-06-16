caa8b9466219162bca50b213b1e8f9e692cbe595
nagachaitanyaappana-cloud <nagachaitanyaappana@gmail.com>
2026-06-16 10:49:32 +0530

    made a rest report controller

--- src/main/java/com/server/backend/controller/ReportController.java
+++ src/main/java/com/server/backend/controller/ReportController.java
@@ -1,39 +1,14 @@
 package com.server.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import com.server.backend.DTO.Reports.DistrictCollegeTypeResponse;
-
-import com.server.backend.DTO.Reports.ItiWithTradesResponse;
-import com.server.backend.service.ReportService;
-
-@RestController
+@Controller
 public class ReportController {
-
-    private final ReportService reportService;
-
-    public ReportController(ReportService reportService) {
-        this.reportService = reportService;
-
-    }
-
-    @PostMapping("/districts/itis-with-trades")
-    public List<ItiWithTradesResponse> getItisWithTradesByDistrict(
-            @RequestBody DistrictCollegeTypeResponse districtCollegeType) {
-        return reportService.getItisWithTradesByDistrict(districtCollegeType);
-    }
 
     @GetMapping("/AboutStrive")
     public String aboutStrive() {
         return "aboutstrive";
     }
 
-    @GetMapping("/DisclosureManagement")
-    public String disclosureManagement() {
-        return "Disclosure Management.";
-    }
 }
