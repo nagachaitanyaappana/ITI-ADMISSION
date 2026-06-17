d90a375b99fdd166896c9872c6250df44922a40e
Ramyamylapalli <mylapalliramya147@gmail.com>
2026-06-17 10:58:54 +0530

    gm

--- src/main/java/com/server/backend/service/ItiServiceImpl.java
+++ src/main/java/com/server/backend/service/ItiServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.BeanUtils;
 import com.server.backend.DTO.Institute.ItiDto;
 import com.server.backend.entity.Iti;
 import com.server.backend.Repository.ItiRepository;
