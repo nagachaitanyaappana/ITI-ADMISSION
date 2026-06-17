b76c6ece482975c9e768f6b95933e59f901c0f23
K.Varshitha <varshithakandregula2008@gmail.com>
2026-06-17 12:01:00 +0530

    2

--- src/main/java/com/server/backend/controller/MeritListController.java
+++ src/main/java/com/server/backend/controller/MeritListController.java
@@ -1,6 +1,5 @@
 package com.server.backend.controller;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import com.server.backend.service.MeritListService;
 @RequestMapping("/api/meritlist")
 public class MeritListController {
 
-    @Autowired
+   
     private MeritListService meritListService;
 
     @GetMapping
