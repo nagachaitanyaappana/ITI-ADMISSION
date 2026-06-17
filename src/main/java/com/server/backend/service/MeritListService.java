1bbd5013fe3e372cfc81d1e6a85c0fd0df77b7c6
Ramyamylapalli <mylapalliramya147@gmail.com>
2026-06-17 11:55:30 +0530

    hii

--- src/main/java/com/server/backend/service/MeritListService.java
+++ src/main/java/com/server/backend/service/MeritListService.java
@@ -11,10 +11,7 @@ import com.server.backend.Repository.MeritListRepository;
 @Service
 public class MeritListService {
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 52ebe2f9e12e4c33d54e06c0d1f5996102fc0df9
     private MeritListRepository meritListRepository;
 
     public List<MeritList> getAllMeritList() {
