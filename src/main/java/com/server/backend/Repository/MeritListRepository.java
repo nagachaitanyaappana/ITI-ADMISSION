90271def4e665240f87dc228cc3e955b38fd91be
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-18 21:04:51 +0530

    kjj

--- src/main/java/com/server/backend/Repository/MeritListRepository.java
+++ src/main/java/com/server/backend/Repository/MeritListRepository.java
@@ -1,9 +1,18 @@
 package com.server.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import com.server.backend.entity.MeritList;
 import com.server.backend.entity.MeritListId;
 
 public interface MeritListRepository extends JpaRepository<MeritList, MeritListId> {
+    MeritList findByRegId(Integer regid);
+     List<MeritList> findByDistCode(String dist_code);
+
+
+    List<MeritList> findByPhase(String phase);
+
+    List<MeritList> findByItiCode(String iti_code);
+
+    List<MeritList> findByAppStatus(String app_status);
 }
--- src/main/java/com/server/backend/service/MeritListService.java
+++ src/main/java/com/server/backend/service/MeritListService.java
@@ -4,7 +4,6 @@ package com.server.backend.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 import com.server.backend.entity.MeritList;
 import com.server.backend.entity.MeritListId;
 import com.server.backend.Repository.MeritListRepository;
@@ -21,6 +20,26 @@ public class MeritListService {
 
     public List<MeritList> getAllMeritList() {
         return meritListRepository.findAll();
+    }
+    
+    public MeritList getMeritListByRegId(Integer regid) {
+        return meritListRepository.findByRegId(regid);
+    }
+
+    public List<MeritList> getMeritListByDistCode(String dist_code) {
+        return meritListRepository.findByDistCode(dist_code);
+    }
+
+    public List<MeritList> getMeritListByPhase(String phase) {
+        return meritListRepository.findByPhase(phase);
+    }
+
+    public List<MeritList> getMeritListByItiCode(String iti_code) {
+        return meritListRepository.findByItiCode(iti_code);
+    }
+
+    public List<MeritList> getMeritListByAppStatus(String app_status) {
+        return meritListRepository.findByAppStatus(app_status);
     }
     public MeritList saveMeritList(MeritList meritList) {
         return meritListRepository.save(meritList);
