0eee2122799913d8898e98328879e565850b0a59
nagachaitanyaappana-cloud <nagachaitanyaappana@gmail.com>
2026-06-19 12:25:24 +0530

    Things are getting  a lot hard

--- src/main/java/com/server/backend/Repository/DistrictMasterRepository.java
+++ src/main/java/com/server/backend/Repository/DistrictMasterRepository.java
@@ -8,14 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import com.server.backend.DTO.Reports.DistrictMasterResponse;
 import com.server.backend.entity.dist_master;
 
-package com.server.backend.Repository;
 
 public interface DistrictMasterRepository extends JpaRepository<dist_master, String> {
 
-    @Query("SELECT e.dist_name FROM dist_master e") // Adjusted to your entity field name
+    @Query("SELECT e.dist_name FROM dist_master e") 
     List<String> findAllNames();
 
-    // Use the exact Entity class name (dist_master) and the fields defined in it
-    @Query("SELECT new com.server.backend.DTO.Reports.DistrictMasterResponse(d.dist_code, d.dist_name) FROM dist_master d")
-   List<DistrictMasterResponse> findAllDistrictsDTO();
+   @Query("SELECT new com.server.backend.DTO.Reports.DistrictMasterResponse(d.dist_code, d.dist_name) FROM dist_master d")
+    List<DistrictMasterResponse> findAllDistrictsDTO();
 }
