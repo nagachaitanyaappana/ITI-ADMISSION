c726c504b14e7f66c849272983c9a4196fe04ca7
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-19 12:02:37 +0530

    h

--- src/main/java/com/server/backend/controller/MeritListController.java
+++ src/main/java/com/server/backend/controller/MeritListController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.server.backend.Repository.MeritListRepository;
 @RestController
 @RequestMapping("/api/meritlist")
 public class MeritListController {
