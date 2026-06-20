2ad89df65dccc9b1b5900f212cf74068a616fd77
K.Varshitha <varshithakandregula2008@gmail.com>
2026-06-20 16:49:52 +0530

    24

--- src/main/java/com/server/backend/controller/trade_names_controller.java
+++ src/main/java/com/server/backend/controller/trade_names_controller.java
@@ -3,7 +3,6 @@ package com.server.backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.server.backend.DTO.admission_process.iti.trade_names_dto;
 import com.server.backend.service.trade_names_service;
 
@@ -11,8 +10,12 @@ import com.server.backend.service.trade_names_service;
 @RequestMapping("/admission")
 public class trade_names_controller {
 
+    private final trade_names_service service;
+
     
-    private trade_names_service service;
+    public trade_names_controller(trade_names_service service) {
+        this.service = service;
+    }
 
     @GetMapping("/master-data")
     public trade_names_dto getMasterData() {
