3b765bb8e9574bb2a3b2e2419805a51bb37248a9
K.Varshitha <varshithakandregula2008@gmail.com>
2026-06-20 16:29:47 +0530

    display trades and itis

---
+++ src/main/java/com/server/backend/DTO/admission_process/iti/trade_names_dto.java
@@ -0,0 +1,30 @@
+package com.server.backend.DTO.admission_process.iti;
+
+import java.util.List;
+
+public class trade_names_dto {
+
+    private List<String> itiNames;
+    private List<String> tradeNames;
+
+    public trade_names_dto(List<String> itiNames, List<String> tradeNames) {
+        this.itiNames = itiNames;
+        this.tradeNames = tradeNames;
+    }
+
+    public List<String> getItiNames() {
+        return itiNames;
+    }
+
+    public void setItiNames(List<String> itiNames) {
+        this.itiNames = itiNames;
+    }
+
+    public List<String> getTradeNames() {
+        return tradeNames;
+    }
+
+    public void setTradeNames(List<String> tradeNames) {
+        this.tradeNames = tradeNames;
+    }
+}
