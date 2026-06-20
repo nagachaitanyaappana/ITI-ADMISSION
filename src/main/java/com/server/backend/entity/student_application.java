bead692dcbcda8daa7f92459dd04d68c9cb8d5da
nagachaitanyaappana-cloud <nagachaitanyaappana@gmail.com>
2026-06-20 13:38:06 +0530

    in the middle of making two new entities

--- src/main/java/com/server/backend/entity/student_application.java
+++ src/main/java/com/server/backend/entity/student_application.java
@@ -9,18 +9,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "student_application", schema = "public")
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class student_application {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "application_registration_id_seq")
     @SequenceGenerator(name = "application_registration_id_seq", sequenceName = "application_Registration_id_seq", allocationSize = 1)
-    @Column(name = "regid", nullable = false)
+    @EqualsAndHashCode.Include
+    @Column(name = "regid", nullable = false, unique = true, updatable = false)
     private Integer regid;
-
-    
 
 }
