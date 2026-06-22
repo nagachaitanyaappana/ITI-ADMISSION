936b1843acc51118a72ab18dc16c56524395f630
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-22 16:53:08 +0530

    repository&service

---
+++ src/main/java/com/server/backend/service/AdmissionTimingService.java
@@ -0,0 +1,24 @@
+package com.server.backend.service;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.server.backend.entity.AdmissionTiming;
+import com.server.backend.Repository.AdmissionTimingRepository;
+@Service
+public class AdmissionTimingService {
+    private final AdmissionTimingRepository admissionTimingRepository;
+    public AdmissionTimingService(AdmissionTimingRepository admissionTimingRepository) {
+        this.admissionTimingRepository = admissionTimingRepository;
+    }
+    public AdmissionTiming createAdmissionTiming(AdmissionTiming admissionTiming) {
+        return admissionTimingRepository.save(admissionTiming);
+    }
+    public List<AdmissionTiming> getAllAdmissionTimings() {
+        return admissionTimingRepository.findAll();
+    }
+    public AdmissionTiming getById(String itiCode) {
+        return admissionTimingRepository.findById(itiCode).orElse(null);
+    }
+    public void delete(String itiCode){
+        admissionTimingRepository.deleteById(itiCode);
+    }
+}
