e7ae77d594d74d5975f7c8de134afa09c70c7a12
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-22 12:56:50 +0530

    added config

---
+++ src/main/java/com/server/backend/Config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.server.backend.Config;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ITI Admission Management System API")
+                        .version("1.0")
+                        .description("This is the backend API documentation.")
+                        .contact(new Contact()
+                                .name("ITI Admission Team")
+                                .email("admission@iti.edu")
+                        )
+                );
+    }
+}
