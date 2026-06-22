880ccc5ab291927aea5db504a7189f02cdcf467c
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-22 13:00:16 +0530

    configuration

--- src/main/java/com/server/backend/config/OpenApiConfig.java
+++ src/main/java/com/server/backend/config/OpenApiConfig.java
@@ -1,8 +1,6 @@
 package com.server.backend.Config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
