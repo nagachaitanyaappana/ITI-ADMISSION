475456a8e3564634f1e2b31494e0afff87feb0a0
mylapalliramya147-ux <mylapalliramya147@gmail.com>
2026-06-23 12:10:15 +0530

    resolving

--- src/main/java/com/server/backend/config/OpenApiConfig.java
+++ src/main/java/com/server/backend/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.server.backend.config;
+package com.server.backend.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
