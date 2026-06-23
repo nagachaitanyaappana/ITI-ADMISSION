b1066d7deefa96c761ab7794e3ef000f4d84a5e7
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-23 22:01:24 +0530

    corrected

--- src/main/java/com/server/backend/Repository/StudentApplicationRepository.java
+++ src/main/java/com/server/backend/Repository/StudentApplicationRepository.java
@@ -3,6 +3,5 @@ package com.server.backend.Repository;
 import com.server.backend.entity.StudentApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentApplicationRepository
-        extends JpaRepository<StudentApplication, Long> {
+public interface StudentApplicationRepository extends JpaRepository<StudentApplication, String> {
 }
