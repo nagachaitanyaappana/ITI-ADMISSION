d77817c7d004715f79b2c61f70d2258e2153d348
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-23 12:40:05 +0530

    undo

--- src/main/java/com/server/backend/entity/Checklist.java
+++ src/main/java/com/server/backend/entity/Checklist.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "checklist2024phase1" , schema = "public")
+@Table(name = "checklist" , schema = "public")
 @Data
 public class Checklist 
 {
