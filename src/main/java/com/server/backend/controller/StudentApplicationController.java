600815789ff0ea4e2f64e54375e0654a19c06c2d
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-25 11:40:45 +0530

    import

--- src/main/java/com/server/backend/controller/StudentApplicationController.java
+++ src/main/java/com/server/backend/controller/StudentApplicationController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PutMapping;
 import com.server.backend.DTO.StudentRegistrationRequest;
 import com.server.backend.entity.StudentApplication;
 import com.server.backend.service.StudentApplicationService;
