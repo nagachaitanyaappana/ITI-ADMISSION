ebc0486516444ca5a63eb4d8fa37408e2408741e
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-06-26 20:38:08 +0530

    added schema to the entity class

--- src/main/java/com/server/backend/entity/AdmissionTiming.java
+++ src/main/java/com/server/backend/entity/AdmissionTiming.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import jakarta.persistence.IdClass;
 @Entity
-@Table(name = "admission_timings")
+@Table(name = "admission_timings" ,schema="public")
 @Data
 @IdClass(AdmissionTimingId.class)
 public class AdmissionTiming {
