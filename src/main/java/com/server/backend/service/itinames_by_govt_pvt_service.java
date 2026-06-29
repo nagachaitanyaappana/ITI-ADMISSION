e8aeeb0f80d4e8b94bb95abf908deca2eb6709ad
K.Varshitha <varshithakandregula2008@gmail.com>
2026-06-29 12:40:19 +0530

    x

---
+++ src/main/java/com/server/backend/service/itinames_by_govt_pvt_service.java
@@ -0,0 +1,20 @@
+
+    package com.server.backend.service;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+
+import com.server.backend.Repository.itinames_by_govt_pvt_repository;
+
+@Service
+public class itinames_by_govt_pvt_service {
+
+  
+    private itinames_by_govt_pvt_repository repository;
+
+    public List<String> getItiNamesByGovtPvt(String govt) {
+        return repository.getItiNamesByGovtPvt(govt);
+    }
+}
