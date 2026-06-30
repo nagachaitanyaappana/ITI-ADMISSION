fe99e9c91859ade053aa77185727a3834ec18172
nagachaitanyaappana-cloud <nagachaitanyaappana@gmail.com>
2026-06-30 15:37:10 +0530

    swagggerrrrr

--- src/main/java/com/server/backend/config/OpenApiConfig.java
+++ src/main/java/com/server/backend/config/OpenApiConfig.java
@@ -1,13 +1,7 @@
 package com.server.backend.config;
-import java.util.Comparator;
-
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerMethod;
-
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -28,42 +22,5 @@ public class OpenApiConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org")));
-    }
-
-    @Bean
-    public GroupedOpenApi reportsApi() {
-        return GroupedOpenApi.builder()
-                .group("reports")
-                .pathsToMatch("/api/reports/**")
-                .build();
-    }
-
-    @Bean
-    public org.springdoc.core.customizers.OperationCustomizer operationCustomizer() {
-        return (Operation operation, HandlerMethod handlerMethod) -> {
-            // Extract number from summary for sorting (e.g., "1 - API Dashboard" -> 1)
-            String summary = operation.getSummary();
-            if (summary != null && !summary.isEmpty()) {
-                try {
-                    int num = Integer.parseInt(summary.split(" - ")[0]);
-                    operation.addExtension("x-order", num);
-                } catch (Exception e) {
-                    // If no number found, assign a high number to sort at the end
-                    operation.addExtension("x-order", 999);
-                }
-            }
-            return operation;
-        };
-    }
-
-    @Bean
-    public Comparator<io.swagger.v3.oas.models.PathItem.HttpMethod> operationSorter() {
-        return (method1, method2) -> {
-            // Sort by HTTP method: GET first, then others
-            if (method1 == method2) return 0;
-            if (method1 == io.swagger.v3.oas.models.PathItem.HttpMethod.GET) return -1;
-            if (method2 == io.swagger.v3.oas.models.PathItem.HttpMethod.GET) return 1;
-            return method1.name().compareTo(method2.name());
-        };
     }
 }
