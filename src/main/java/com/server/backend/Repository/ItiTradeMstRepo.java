b526d24d5561fcae21a7af84687b153856a309da
mylapalliramya147-ux <mylapalliramya147@gmail.com>
2026-06-30 11:02:50 +0530

    modifying

--- src/main/java/com/server/backend/Repository/ItiTradeMstRepo.java
+++ src/main/java/com/server/backend/Repository/ItiTradeMstRepo.java
@@ -5,7 +5,6 @@ package com.server.backend.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.server.backend.entity.Iti_trade_mst;
 
-public interface ItiTradeMstRepo
-        extends JpaRepository<Iti_trade_mst, String> {
+public interface ItiTradeMstRepo extends JpaRepository<Iti_trade_mst, String> {
 
 }
